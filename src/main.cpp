eb96c05b1aa05c2a040dfa79dcdf3c904388ae88
liyixin-isv <liyixin.qaq@gmail.com>
2024-02-22 11:00:03 +0800

    新增单元测试示例

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
+#include "spdlog/fmt/ranges.h"
 #include "spdlog/spdlog.h"
-#include <iostream>
 #include <string_view>
+#include <vector>
+void foo(int a) {
+  for (size_t i = 0; i < a; i++) {
+    std::string_view s{"Hello world!"};
+    spdlog::info("{}", s.data());
+  }
+}
 int main(int argc, char *argv[]) {
 
-  std::string_view s{"Hello world!"};
-  spdlog::info("{}", s.data());
+  foo(10);
+  std::vector<int> v;
+  v.push_back(1);
+  v.push_back(1);
+  v.push_back(1);
 
+  SPDLOG_INFO("{}", v);
   return 0;
 }
